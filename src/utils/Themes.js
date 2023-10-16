bd96277b9e35572be5c51bbe0cbb53b7953a2f42
pallavi-sahu <pallavisahu0610@gmail.com>
2023-10-17 00:53:36 +0530

    dark and light theme

--- src/utils/Themes.js
+++ src/utils/Themes.js
@@ -1,24 +1,24 @@
 const darktheme = {
-    bg: "#1C1C27",
+    bg:"#1C1C27",
     bgLight: "#1C1E27",
-    primary: "#854CE6",
-    text_primary: "#F2F3F4",
-    text_secondary: "b1b2b3",
-    card: "#171721",
-    card_light: "#191924",
-    button: "#854CE6",
-    white: "#FFFFFF",
-    black: "#000000",
+    primary:"#854CE6",
+    text_primary:"#F2F3F4",
+    text_secondary:"#b1b2b3",
+    card:"#171721",
+    card_light: '#191924',
+    button:"#854CE6",
+    white:"#FFFFFF",
+    black:"#000000",
 }; 
 
 const lighttheme = {
-    bg: "#FFFFFF",
+    bg:"#FFFFFF",
     bgLight: "#f0f0f0",
-    primary: "#be1adb",
-    text_primary: "#111111",
-    text_secondary: "48494a",
-    card: "#FFFFFF",
-    button: "#5c5b5b",
+    primary:"#be1adb",
+    text_primary:"#111111",
+    text_secondary:"#48494a",
+    card:"#FFFFFF",
+    button:"#5c5b5b",
 }
 
 export {lighttheme, darktheme};
